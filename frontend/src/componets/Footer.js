c23a6800fbccbc19cfc4e5b836af2cf8dd6f3033
Nishanthfs <152601019+Nishanthfs@users.noreply.github.com>
2024-08-26 20:16:30 +0530

    Update Footer.js

--- frontend/src/componets/Footer.js
+++ frontend/src/componets/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <route>
-      <footer className='bg-slate-300'>
+      <footer className='bg-slate-300 mt-14'>
       <div className='container mx-auto p-4'>
         <p className='text-center font-bold' title='YouTube Channel'>Dynamic coder NH</p>
       </div>
